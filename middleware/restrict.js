3ca3d7ea81a5d0f821e33b4d52c393a36fbe00da
mandihamza <mandihamza@gmail.com>
2020-03-24 17:12:42 -0600

    Adds middleware to authenticate users at login (MVP req.)

---
+++ middleware/restrict.js
@@ -0,0 +1,34 @@
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const Users = require("../users/users-model")
+
+function restrict() {
+	const authError = {
+		message: "Invalid credentials",
+	}
+	
+	return async (req, res, next) => {
+		try {
+			const { token } = req.cookies
+
+			if (!token) {
+				return res.status(401).json(authError)
+			}
+
+			jwt.verify(token, process.env.JWT_SECRET || "secret", 
+			(err, decoded) => {
+				if (err) {
+					return res.status(401).json(authError)
+				}
+
+				req.token = decoded
+
+				next()
+			})
+		} catch(err) {
+			next(err)
+		}
+	}
+}
+
+module.exports = restrict
